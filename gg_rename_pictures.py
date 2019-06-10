ff4f84f0cc437e3384c6d2f2f2474fece8bd06bb
Julie Digne <jdigne@gmail.com>
2019-06-10 09:34:28 +0200

    final script

--- gg_rename_pictures.py
+++ gg_rename_pictures.py
@@ -11,26 +11,39 @@ def get_date_name(image):
     """Read exif data from image to return string img name with timestamp"""
     ext = os.path.splitext(image)[1][1:]
     exifdico =  Image.open(image)._getexif()
+    abspath = os.path.dirname(image)
+    exists = False
+
     if exifdico is None or 36867 not in exifdico.keys():
-        datename = "2500_" + os.path.basename(image)
-        print("No Exif data for %s" % (image))
+        name = os.path.basename(image)
+        name_without_ext = os.path.splitext(name)[0]
+        if name_without_ext.startswith("2500_") :
+            date = name_without_ext
+        else :
+            date = "2500_" + name_without_ext
+        print("No Exif data for %s using name %s" % (image,date))
+        exists =True
     else :
         date =  exifdico[36867]
         date = date.replace(":", "")
         date = date.replace(" ", "_")
 
-
-    datename = date + "." + ext
+    tempname = date + "." + ext
+    datename = os.path.join(abspath, tempname)
+    
     if os.path.exists(datename):
         im1 = Image.open(image)
-        counter = 1
-        while os.path.exists(datename):
+        counter = 0
+        while os.path.exists(datename) and exists==False :
             im2 = Image.open(datename)
-            if hashlib.md5(im1) == hashlib.md5(im2):
+            if hashlib.md5(im1.tobytes()).hexdigest() != hashlib.md5(im2.tobytes()).hexdigest():
                 counter = counter + 1
-                datename = "%s_%02d.%s" %(date, counter, ext)
+                tempname = "%s_%02d.%s" %(date, counter, ext)
+                datename = os.path.join(abspath, tempname)
+            else :
+                exists=True
 
-        newname = "%s_%02d.%s" %(newname, counter,ext)
+    return exists,datename
 
 def file_has_ext(f, ext_list):
     return os.path.splitext(f)[1][1:] in ext_list
@@ -47,9 +60,10 @@ def main():
     filelist = [f for f in os.listdir(args.img_folder) if file_has_ext(f,img_ext)]
     for img in filelist:
         fileimg = os.path.join(args.img_folder, img)
-        datename = get_date_name(fileimg)
+        fileexists,datename = get_date_name(fileimg)
         newname = os.path.join(args.img_folder, datename)
-        print("processing %s -> %s" % (img, newname))
-        os.rename(fileimg,newname)
+        if newname != fileimg :
+            print("processing %s -> %s" % (img, newname))
+            os.rename(fileimg,newname)
 
 main()
